2292ec2a736edcc05d53e724c3d112b357308afd
Fenotiana <m.andriamahenimanana@etechconsulting-mg.com>
2021-10-05 20:37:33 +0300

    return process.Env

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { PORT: port = 3000 } = process.env;
 const app = express();
 
-app.get("/", (req, res) => res.json({ message: "ok" }));
+app.get("/", (req, res) => res.json({ message: process.env }));
 
 app.listen(port);
 module.exports = app;
