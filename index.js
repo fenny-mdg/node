83a90f86047aa7c62460c25089d7d836b7b6562a
Fenotiana <m.andriamahenimanana@etechconsulting-mg.com>
2021-10-05 19:45:36 +0300

    Add get route

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const express = require("express");
 const { PORT: port = 3000 } = process.env;
 const app = express();
 
+app.get("/", (req, res) => res.json({ message: "ok" }));
+
 app.listen(port, () => console.log(`Listening ${port}`));
